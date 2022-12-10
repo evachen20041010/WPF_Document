54965b9797dd1b90ba7fe099476915bafdb52a4d
evachen20041010 <a0937510149@gmail.com>
2022-12-11 02:24:13 +0800

    20221128_C

---
+++ WPF_Document/MyDocumentViewer.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Document
+{
+    /// <summary>
+    /// MyDocumentViewer.xaml 的互動邏輯
+    /// </summary>
+    public partial class MyDocumentViewer : Window
+    {
+        public MyDocumentViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            object property = rtbEditor.Selection.GetPropertyValue(Inline.FontWeightProperty);
+            btnBold.IsChecked = (property != DependencyProperty.UnsetValue) && (property.Equals(FontWeights.Bold));
+
+            property = rtbEditor.Selection.GetPropertyValue(Inline.FontStyleProperty);
+            btnItalic.IsChecked = (property != DependencyProperty.UnsetValue) && (property.Equals(FontStyles.Italic));
+
+            property = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            btnUnderline.IsChecked = (property != DependencyProperty.UnsetValue) && (property.Equals(TextDecorations.Underline));
+        }
+    }
+}
