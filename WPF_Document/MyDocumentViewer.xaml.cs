344a62c2ac53e6e140976781154c64383582a755
evachen20041010 <a0937510149@gmail.com>
2022-12-11 21:24:43 +0800

    20221205_B

--- WPF_Document/MyDocumentViewer.xaml.cs
+++ WPF_Document/MyDocumentViewer.xaml.cs
@@ -1,16 +1,11 @@
-﻿using System;
+﻿using Microsoft.Win32;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace WPF_Document
 {
@@ -19,6 +14,7 @@ namespace WPF_Document
     /// </summary>
     public partial class MyDocumentViewer : Window
     {
+        Color fontColor = Colors.Black;
         public MyDocumentViewer()
         {
             InitializeComponent();
@@ -29,9 +25,10 @@ namespace WPF_Document
             cmbFontFamily.SelectedIndex = 0;
             cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 24, 32, 40, 50, 60, 80, 100 };
             cmbFontSize.SelectedIndex = 4;
+            fontColorPicker.SelectedColor = fontColor;
         }
 
-        private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        private void Editor_SelectionChanged(object sender, RoutedEventArgs e)
         {
             object property = rtbEditor.Selection.GetPropertyValue(Inline.FontWeightProperty);
             btnBold.IsChecked = (property != DependencyProperty.UnsetValue) && (property.Equals(FontWeights.Bold));
@@ -47,9 +44,15 @@ namespace WPF_Document
 
             property = rtbEditor.Selection.GetPropertyValue(Inline.FontSizeProperty);
             cmbFontSize.SelectedItem = property;
+
+            SolidColorBrush? foregroundProperty = rtbEditor.Selection.GetPropertyValue(Inline.ForegroundProperty) as SolidColorBrush;
+            if (foregroundProperty != null)
+            {
+                fontColorPicker.SelectedColor = foregroundProperty.Color;
+            }
         }
 
-        private void cmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void FontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (cmbFontFamily.SelectedItem != null)
             {
@@ -57,12 +60,54 @@ namespace WPF_Document
             }
         }
 
-        private void cmbFontSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void FontSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (cmbFontSize.SelectedItem != null)
             {
                 rtbEditor.Selection.ApplyPropertyValue(TextElement.FontSizeProperty, cmbFontSize.SelectedItem);
             }
+        }
+
+        private void FontColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            fontColor = (Color)e.NewValue;
+            SolidColorBrush colorBrush = new SolidColorBrush(fontColor);
+            rtbEditor.Selection.ApplyPropertyValue(TextElement.ForegroundProperty, colorBrush);
+        }
+
+        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Rich Text Format file|*.ref|所有檔案|*.*";
+            if (fileDialog.ShowDialog() == true)
+            {
+                FileStream fs = new FileStream(fileDialog.FileName, FileMode.Open);
+                TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+                range.Load(fs, DataFormats.Rtf);
+            }
+        }
+
+        private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog fileDialog = new SaveFileDialog();
+            fileDialog.Filter = "Rich Text Format file|*.ref|所有檔案|*.*";
+            if (fileDialog.ShowDialog() == true)
+            {
+                FileStream fs = new FileStream(fileDialog.FileName, FileMode.Create);
+                TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+                range.Save(fs, DataFormats.Rtf);
+            }
+        }
+
+        private void New_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MyDocumentViewer myDocumentViewer = new MyDocumentViewer();
+            myDocumentViewer.Show();
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            rtbEditor.Document.Blocks.Clear();
         }
     }
 }
